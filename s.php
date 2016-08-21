d241615ea4d487c20736ca3062039070a69cff8e
laklek <lesleydk.ldk@gmail.com>
2016-08-21 08:36:09 +0200

    Add files via upload

---
+++ s.php
@@ -0,0 +1,84 @@
+<?php
+
+$dbHost = "localhost";
+$dbUser = "root";
+$dbPass = "";
+$dbName = "";
+
+$mysqli = new mysqli($dbHost, $dbUser, $dbPass, $dbName); //connect to the MySQL remote server using the improved MySQL wrapper. Syntax: $host, $username, $pass, $db
+
+
+$shortUrl = $_GET['u']; // $_GET['u'] returns the full path without the protocol (http, https) so we'll have to remove that
+
+if($shortUrl) {
+
+    $longUrl = getLongUrl($shortUrl);
+    if (!$longUrl) {
+        header("Location: /"); //invalid short url, returning to home page.
+        exit; //Prevent the script from executing further if somehow client blocked redirects.
+    }
+
+    header("Location: " . $longUrl);  //Redirect to longer Url. You could implement a check here but I don't want to cause I want to use
+    exit();                             //custom host names like http://workstation or ftp://myPi. Not recommended to use this for public use.
+
+}elseif($_GET['short']||$_GET['long']){
+    die(registerShortUrl($_GET['short'], $_GET['long'])); //Both GET and POST support
+}elseif($_POST['short']||$_POST['long']) {
+    die(registerShortUrl($_POST['short'], $_POST['long']));
+}elseif($_GET["del"]){
+    unRegister($_GET["del"]);
+}elseif($_POST["del"]){
+    unRegister($_POST["del"]);
+}else{
+    header("Location: /"); //You're drunk, go home.
+    exit;
+}
+
+function registerShortUrl($short, $long){
+    global $mysqli;
+
+    if(strlen($short) > 0 && strlen($long) > 0){
+
+    $mysqlLongUrlQry = $mysqli->query("SELECT * FROM short WHERE shortUrl='$short'");
+    if ($mysqlLongUrlQry->num_rows == 0) {
+        if($mysqli->query("INSERT INTO `short` (`shortUrl`, `longUrl`) VALUES ('$short', '$long')")){
+            return "ok";
+        }else{
+            return "Error while inserting.";
+        }
+    }else{
+        $longUrl = getLongUrl($short);
+        return "Error: $short already taken. Points to: <a href=" . '"' . $longUrl . '"' . ">$longUrl</a> .";
+    }
+    }else{
+        return "Please fill both the short and long URL in.";
+    }
+}
+
+function unRegister($short){
+    $mysqlDelQry = $mysqli->query("DELETE FROM short WHERE shortUrl='$short'");
+    if($mysqlDelQry === false){
+        return "An error occurred.";
+    }else{
+        return "ok";
+    }
+}
+
+function getLongUrl($short){
+    global $mysqli;
+
+    $mysqlLongUrlQry = $mysqli->query("SELECT * FROM short WHERE shortUrl='$short'");
+    if ($mysqlLongUrlQry->num_rows == 0) {
+        return false;
+    }else{
+        $longUrl = $mysqlLongUrlQry->fetch_assoc()["longUrl"];
+        return $longUrl;
+    }
+}
+
+function addHttp($url) { //making sure it's an absolute path
+    if (!preg_match("/[a-zA-Z]+:\/\//", $url)) {
+        $url = "http://" . $url;
+    }
+    return $url;
+}
---
+++ short.sql
@@ -0,0 +1,59 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Aug 21, 2016 at 06:03 AM
+-- Server version: 5.5.50-0+deb8u1
+-- PHP Version: 5.6.24-0+deb8u1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `lesley_xyz`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `short`
+--
+
+CREATE TABLE `short` (
+  `id` int(11) NOT NULL,
+  `shortUrl` varchar(255) NOT NULL,
+  `longUrl` varchar(2084) NOT NULL,
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `short`
+--
+ALTER TABLE `short`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `shortUrl` (`shortUrl`),
+  ADD UNIQUE KEY `id` (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `short`
+--
+ALTER TABLE `short`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
