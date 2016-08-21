e9678c8b73e47d2d1c18576f23a3ffe395b9db7e
laklek <lesleydk.ldk@gmail.com>
2016-08-21 09:07:45 +0200

    Bug fixes

--- s.php
+++ s.php
@@ -10,7 +10,7 @@ $mysqli = new mysqli($dbHost, $dbUser, $dbPass, $dbName); //connect to the MySQL
 
 $shortUrl = $_GET['u']; // $_GET['u'] returns the full path without the protocol (http, https) so we'll have to remove that
 
-if($shortUrl) {
+if ($shortUrl) {
 
     $longUrl = getLongUrl($shortUrl);
     if (!$longUrl) {
@@ -18,67 +18,72 @@ if($shortUrl) {
         exit; //Prevent the script from executing further if somehow client blocked redirects.
     }
 
-    header("Location: " . $longUrl);  //Redirect to longer Url. You could implement a check here but I don't want to cause I want to use
-    exit;                           //custom host names like http://workstation or ftp://myPi. Not recommended to use this for public use.
+    $mysqli->query("UPDATE short SET hits = hits + 1 WHERE shortUrl = '$shortUrl'"); //Register Hit
+    header("Location: " . addHttp($longUrl));    //Redirect to longer Url. You could implement a check here but I don't want to cause I want to use
+    exit;                               //custom host names like http://workstation or ftp://myPi. Not recommended to use this for public use.
 
-}elseif($_GET['short']||$_GET['long']){
+} elseif ($_GET['short'] || $_GET['long']) {
     die(registerShortUrl($_GET['short'], $_GET['long'])); //Both GET and POST support
-}elseif($_POST['short']||$_POST['long']) {
+} elseif ($_POST['short'] || $_POST['long']) {
     die(registerShortUrl($_POST['short'], $_POST['long']));
-}elseif($_GET["del"]){
+} elseif ($_GET["del"]) {
     die(unRegister($_GET["del"]));
-}elseif($_POST["del"]){
+} elseif ($_POST["del"]) {
     die(unRegister($_POST["del"]));
-}else{
+} else {
     header("Location: /"); //You're drunk, go home.
     exit;
 }
 
-function registerShortUrl($short, $long){
+function registerShortUrl($short, $long)
+{
     global $mysqli;
 
-    if(strlen($short) > 0 && strlen($long) > 0){
+    if (strlen($short) > 0 && strlen($long) > 0) {
 
-    $mysqlLongUrlQry = $mysqli->query("SELECT * FROM short WHERE shortUrl='$short'");
-    if ($mysqlLongUrlQry->num_rows == 0) {
-        if($mysqli->query("INSERT INTO `short` (`shortUrl`, `longUrl`) VALUES ('$short', '$long')")){
-            return "ok";
-        }else{
-            return "Error while inserting.";
+        $mysqlLongUrlQry = $mysqli->query("SELECT * FROM short WHERE shortUrl='$short'");
+        if ($mysqlLongUrlQry->num_rows == 0) {
+            if ($mysqli->query("INSERT INTO `short` (`shortUrl`, `longUrl`) VALUES ('$short', '$long')")) {
+                return "ok";
+            } else {
+                return "Error while inserting.";
+            }
+        } else {
+            $longUrl = getLongUrl($short);
+            return "Error: $short already taken. Points to: <a href=" . '"' . $longUrl . '"' . ">$longUrl</a> .";
         }
-    }else{
-        $longUrl = getLongUrl($short);
-        return "Error: $short already taken. Points to: <a href=" . '"' . $longUrl . '"' . ">$longUrl</a> .";
-    }
-    }else{
+    } else {
         return "Please fill both the short and long URL in.";
     }
 }
 
-function unRegister($short){
+function unRegister($short)
+{
     global $mysqli;
 
     $mysqlDelQry = $mysqli->query("DELETE FROM short WHERE shortUrl='$short'");
-    if($mysqlDelQry === false){
+    if ($mysqlDelQry === false) {
         return "An error occurred.";
-    }else{
+    } else {
         return "ok";
     }
 }
 
-function getLongUrl($short){
+function getLongUrl($short)
+{
     global $mysqli;
 
     $mysqlLongUrlQry = $mysqli->query("SELECT * FROM short WHERE shortUrl='$short'");
     if ($mysqlLongUrlQry->num_rows == 0) {
         return false;
-    }else{
+    } else {
         $longUrl = $mysqlLongUrlQry->fetch_assoc()["longUrl"];
         return $longUrl;
     }
 }
 
-function addHttp($url) { //making sure it's an absolute path
+function addHttp($url)
+{ //making sure it's an absolute path
     if (!preg_match("/[a-zA-Z]+:\/\//", $url)) {
         $url = "http://" . $url;
     }
